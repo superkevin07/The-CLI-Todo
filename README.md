2cc295096a3adfe1438e07edfe57450003040730
Super Byte <76512557+SuperByte-github@users.noreply.github.com>
2020-12-27 17:34:07 +0530

    intial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+***WIP***
+
+***
+
+### ABOUT
+
+This is just a simple tool to generate a **Markdown** file as a To-Do list using the console.
+
+***
+
+### HOW TO USE IT
+
+Just run the .exe and it will initialize then it will ask you to type something, The typings of yours will be marked as **"[USER] "**.
+
+***
---
+++ todo.py
@@ -0,0 +1,24 @@
+from time import sleep
+from colorama import Fore, Style, init
+init(autoreset = True)
+
+
+print(f"{Fore.CYAN}{Style.BRIGHT}[CONSOLE] initializing...")
+sleep(3)
+file = open("todo.md", "w+")
+print(f"{Fore.CYAN}{Style.BRIGHT}[CONSOLE] there we go...")
+sleep(1.5)
+print(f"\n\n{Fore.CYAN}{Style.BRIGHT}[CONSOLE] Todo")
+
+def check():
+	uin = input("[USER] ")
+
+	if uin != "done@":
+		file.write("\n- [X] " + uin)
+		check()
+	else:
+		print(f"\n{Fore.CYAN}{Style.BRIGHT}[CONSOLE] finished, now you can open the markdown file genertated and uncheck all of the boxes and cut paste the file in your desired location.\nNOTE : The file is named as 'todo.md'.")
+		print(f"\n{Fore.CYAN}{Style.BRIGHT}[CONSOLE] Press 'Enter' to quit...")
+		input()
+
+check()
