059653ec25316b298e842b0e06fd032602fcc0bb
Super Byte <76512557+SuperByte-github@users.noreply.github.com>
2020-12-28 11:31:20 +0530

    reupload bug free program

--- theclitodo.py
+++ theclitodo.py
@@ -15,7 +15,7 @@ def check():
 	uin = input("[USER] ")
 
 	if uin != "done@":
-		file.write("\n- [] " + uin)
+		file.write("\n- [ ] " + uin)
 		check()
 	else:
 		print(f"\n{Fore.CYAN}{Style.BRIGHT}[CONSOLE] finished, now you can open the markdown file genertated and uncheck all of the boxes if already checked and cut paste the file in your desired location,Make sure you have a markdown reader in your computer.\nNOTE : The file is named as 'todo.md'.")
